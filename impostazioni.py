a724362e31b3fd8565efe082f9ac04fd6bd919d6
AleLuzzi <alessandro.luzzi77@gmail.com>
2021-05-17 12:11:36 +0200

    sostituito ugalaxy con ishuttle

--- impostazioni.py
+++ impostazioni.py
@@ -14,23 +14,23 @@ class Impostazioni(tk.Frame):
         self.winswgx_dir = tk.StringVar()
         self.winswgx_dir.set(self.config['Winswgx']['dir'])
 
-        self.lbl_ugalaxy_value = tk.StringVar()
-        self.lbl_ugalaxy_value.set(self.config['iShuttle']['dir'])
+        self.lbl_ishuttle_value = tk.StringVar()
+        self.lbl_ishuttle_value.set(self.config['iShuttle']['dir'])
 
         self.lbl_pyinsta_value = tk.StringVar()
         self.lbl_pyinsta_value.set(self.config['PyInsta']['dir'])
 
-        # LABELFRAME UGALAXY
-        self.lblfrm_ugalaxy = tk.LabelFrame(self, text='Ugalaxy', foreground='blue')
+        # LABELFRAME iShuttle
+        self.lblfrm_ishuttle = tk.LabelFrame(self, text='iShuttle', foreground='blue')
 
-        # LABEL Ugalaxy
-        self.btn_ugalaxy = tk.Button(self.lblfrm_ugalaxy,
-                                     text='Ugalaxy Folder',
-                                     command=self.ugalaxy_open_dir)
+        # LABEL iShuttle
+        self.btn_ishuttle = tk.Button(self.lblfrm_ishuttle,
+                                      text='iShuttle Folder',
+                                      command=self.ishuttle_open_dir)
 
-        self.lbl_ugalaxy = tk.Label(self.lblfrm_ugalaxy,
-                                    textvariable=self.lbl_ugalaxy_value,
-                                    relief='sunken')
+        self.lbl_ishuttle = tk.Label(self.lblfrm_ishuttle,
+                                     textvariable=self.lbl_ishuttle_value,
+                                     relief='sunken')
 
         # LABELFRAME PyInsta
         self.lblfrm_pyinsta = tk.LabelFrame(self, text='PyInsta Folder', foreground='blue')
@@ -57,12 +57,12 @@ class Impostazioni(tk.Frame):
                                     relief='sunken')
 
         # LAYOUT
-        self.lblfrm_ugalaxy.grid(row=1, column=0)
+        self.lblfrm_ishuttle.grid(row=1, column=0)
         self.lblfrm_pyinsta.grid(row=2, column=0, sticky='we')
         self.lblfrm_winswgx.grid(row=3, column=0, sticky='we')
 
-        self.btn_ugalaxy.grid()
-        self.lbl_ugalaxy.grid()
+        self.btn_ishuttle.grid()
+        self.lbl_ishuttle.grid()
 
         self.btn_pyinsta.grid()
         self.lbl_pyinsta.grid()
@@ -83,12 +83,12 @@ class Impostazioni(tk.Frame):
         self.config.write(cfg_file)
         self.winswgx_dir.set(new_dirname)
 
-    def ugalaxy_open_dir(self):
+    def ishuttle_open_dir(self):
         new_dirname = filedialog.askdirectory(parent=self, initialdir='c:\\')
         cfg_file = open('config.ini', 'w')
         self.config.set('Ugalaxy', 'dir', new_dirname)
         self.config.write(cfg_file)
-        self.lbl_ugalaxy_value.set(new_dirname)
+        self.lbl_ishuttle_value.set(new_dirname)
 
     def pyinsta_open_dir(self):
         new_dirname = filedialog.askdirectory(parent=self, initialdir='c:\\')
